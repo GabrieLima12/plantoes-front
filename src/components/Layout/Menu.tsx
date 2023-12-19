16cd347a90fc851ebb66081e9e430b50508c8865
GabrieLima12 <gabrieldocouto.1207@gmail.com>
2023-12-18 23:24:02 -0300

    Criando design da pagina de cadastro

--- src/components/Layout/Menu.tsx
+++ src/components/Layout/Menu.tsx
@@ -1,10 +1,18 @@
+import { Link } from "react-router-dom";
 import styles from "./Menu.module.css";
 
 const Menu = () => {
 
   return (
     <div className={styles.container}>
-        
+        <div className={styles.links} >
+          <Link to="/" className={styles.link} >
+            Listagem Plantonistas
+          </Link>
+          <Link to="/cadastrar-plantonista" className={styles.link} >
+            Cadastrar Plantonistas
+          </Link>
+        </div>
     </div>
   )
 }
--- src/pages/CadastrarPlantonista.tsx
+++ src/pages/CadastrarPlantonista.tsx
@@ -1,9 +1,51 @@
+import { Button, Input } from 'antd';
 import styles from './CadastrarPlantonista.module.css';
+import { ChangeEvent, useState } from 'react';
 
 const CadastrarPlantonista = () => {
+
+  const [nomeMedico, setNomeMedico] = useState<string>("");
+  const [especialidade, setEspecialidade] = useState<string>("");
+  const [crm, setCrm] = useState<string>("");
+
+  const handleCrmChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const novoValor = e.target.value.replace(/\D/g, '');
+    setCrm(novoValor);
+  }
+
   return (
     <div className={styles.container}>
-      CadastrarPlantonista
+        <h1 className={styles.titulo} >Cadastro de Médico Plantonista</h1>
+        <div className={styles.inputs}>
+          <label><strong>CRM do Médico:</strong></label>
+          <Input
+            type='text'
+            value={crm}
+            onChange={handleCrmChange}
+            maxLength={6}
+            className={styles.input_crm} />
+        </div>
+        <div className={styles.inputs}>
+          <label><strong>Epecialidade do Médico:</strong></label>
+          <Input 
+            value={especialidade}
+            onChange={(e) => setEspecialidade(e.target.value)}
+            maxLength={150}
+            type='text'
+            className={styles.input_especialidade} />
+        </div>
+        <div className={styles.inputs}>
+          <label><strong>Nome do Médico:</strong></label>
+          <Input
+            value={nomeMedico}
+            onChange={(e) => setNomeMedico(e.target.value)} 
+            maxLength={150}
+            type='text'
+            className={styles.input_nome} />
+        </div>
+        <div className={styles.botao_rodape}>
+          <Button>Enviar</Button>
+        </div>
     </div>
   )
 }
